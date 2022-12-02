a29b2c391bd349e39c7fe8b6ced655a60bce8d84
lsalgado <ls140511@gmail.com>
2022-12-02 11:35:58 -0500

    process.env.port added to the index.js and start script ready

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ app.get("/about", (req, res) => {
     res.send('<h1>About</h1>')
 });
 
-app.listen(3000);
-console.log("Server on port 3000");
+app.listen(process.env.port || 3000);
+console.log("Server on port 3000", process.env.port || 3000);
